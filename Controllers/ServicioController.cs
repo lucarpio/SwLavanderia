b2a53925758173efe67b89c5ff45b5f080b2d6b9
lucarpio <christian_carpio1@usmp.pe>
2020-12-10 16:47:36 -0300

    backend servicios

--- Controllers/ServicioController.cs
+++ Controllers/ServicioController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SwLavanderia.Data;
 using SwLavanderia.Models;
@@ -7,48 +9,64 @@ namespace SwLavanderia.Controllers
     public class ServicioController : Controller
     {
         private readonly LavanderiaContext _context;
-        public ServicioController(LavanderiaContext c)
+        private readonly UtilsController _utilitario;
+        public ServicioController(LavanderiaContext c, UtilsController u)
         {
             _context = c;
+            _utilitario = u;
         }
+
+
 
         public IActionResult Registro()
         {
             //aca se ve el formulario de registro de almacen
             return View();
         }
-
         [HttpPost]
-        public IActionResult Registro(Servicio serv)
+        public IActionResult Registro(Ticket objTk)
         {
             ViewBag.axcel = "axcel";
-            //aca se registra el nuevo servicio(ticket) a guardar en almacen
-            return View("Detalle",serv);
+            //aca se registra el/ nuevo servicio(ticket) a guardar en almacen
+            ViewBag.clientes = _utilitario.listarClientes();
+            objTk.TkFechaIngreso = System.DateTime.Now;
+            objTk.EstadoId = 1;
+            _context.Add(objTk);
+            _context.SaveChanges();
+            return View("Detalle",objTk);
         }
 
+
+
+        public IActionResult Detalle(Ticket objTk)
+        {
+            //recibe los datos para mostrar info del ticket al que se le añaden los servicios
+            return View();
+        }
+        [HttpPost]
         public IActionResult Detalle(Servicio detServ)
         {
-            //aca se añaden los servicios al ticket
-            //se debe poder reutilizar varias veces
+            _context.Add(detServ);
+            _context.SaveChanges();
             return View();
         }
 
+
+
+        public IActionResult Entrega(Ticket objTk)
+        {
+            //se añade la fecha de entrega al ticket
+            _context.Update(objTk);
+            _context.SaveChanges();
+            return View();
+        }
         [HttpPost]
-        public IActionResult Detallep(Servicio detServ)
+        public IActionResult Entrega()
         {
             return View();
         }
 
-        public IActionResult Entrega(Servicio detServ)
-        {
-            return View();
-        }
-        
-        [HttpPost]
-        public IActionResult Entregap(Servicio detServ)
-        {
-            return View();
-        }
+
 
         public IActionResult Confirmacion()
         {
