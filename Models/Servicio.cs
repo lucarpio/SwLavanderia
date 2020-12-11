c76130971c7b32517db74d7b3d92f964ed42f7e7
lucarpio <christian_carpio1@usmp.pe>
2020-12-11 18:48:51 -0300

    correcciones db

--- Models/Servicio.cs
+++ Models/Servicio.cs
@@ -6,8 +6,11 @@ namespace SwLavanderia.Models
     public class Servicio
     {
         public int Id { get; set; }
-        public string NomServ { get; set; }
+        public ServiciosDisponibles serviciosDisponibles { get; set; }
+        public int serviciosDisponiblesId { get; set; }
         public string DescServicio { get; set; }
         public double PrecServicio { get; set; }
+        public Ticket ticket { get; set; }
+        public int TicketId { get; set; }
     }
 }
